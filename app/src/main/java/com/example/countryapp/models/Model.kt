296a5eb453b31cabc38d31cb49af1adfbc70e1f3
ferhatseker180 <ferhatseker180@gmail.com>
2023-08-06 16:35:58 +0300

    CountrApp Uygulamasına Başlandı.

---
+++ app/src/main/java/com/example/countryapp/models/Model.kt
@@ -0,0 +1,13 @@
+package com.example.countryapp.models
+
+
+
+
+data class Country(
+    val countryName : String?,
+    val countryCapital : String?,
+    val countryRegion : String?,
+    val countryCurrency : String?,
+    val flagUrl : String?,
+    val language : String?)
+
